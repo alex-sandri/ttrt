08622fe453a8d527c197c97f9198e11ac9324c92
Alex Sandri <me@alexsandri.com>
2022-07-01 10:56:34 +0200

    update settings page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:ttrt/bloc/game/bloc.dart';
 import 'package:ttrt/pages/game.dart';
 import 'package:ttrt/pages/settings.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,42 +13,6 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('TTRT'),
         actions: [
-          FutureBuilder<PackageInfo>(
-              future: PackageInfo.fromPlatform(),
-              builder: (context, snapshot) {
-                final PackageInfo? packageInfo = snapshot.data;
-
-                if (packageInfo == null) {
-                  return const SizedBox();
-                }
-
-                return IconButton(
-                  onPressed: () => showAboutDialog(
-                    context: context,
-                    applicationName: packageInfo.appName,
-                    applicationVersion: packageInfo.version,
-                    children: [
-                      ListTile(
-                        title: const Text('View on GitHub'),
-                        trailing: const Icon(Icons.open_in_new),
-                        onTap: () async {
-                          final Uri url =
-                              Uri.parse('https://github.com/alex-sandri/ttrt');
-
-                          if (await canLaunchUrl(url)) {
-                            await launchUrl(
-                              url,
-                              mode: LaunchMode.externalApplication,
-                            );
-                          }
-                        },
-                      ),
-                    ],
-                  ),
-                  tooltip: 'Info',
-                  icon: const Icon(Icons.info),
-                );
-              }),
           IconButton(
             onPressed: () => Navigator.of(context)
                 .push(MaterialPageRoute(builder: (_) => const SettingsPage())),
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -6,8 +8,69 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(),
+      appBar: AppBar(title: const Text('Settings')),
+      body: ListView(
+        children: [
+          FutureBuilder<PackageInfo>(
+            future: PackageInfo.fromPlatform(),
+            builder: (context, snapshot) {
+              final PackageInfo? packageInfo = snapshot.data;
+
+              if (packageInfo == null) {
+                return const SizedBox();
+              }
+
+              return ListTile(
+                leading: const Icon(Icons.info),
+                title: const Text('Open source licenses'),
+                trailing: const Icon(Icons.arrow_forward),
+                onTap: () => showLicensePage(
+                  context: context,
+                  applicationName: packageInfo.appName,
+                  applicationVersion: packageInfo.version,
+                  applicationIcon: const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: CircleAvatar(
+                      foregroundImage: AssetImage(
+                        'assets/images/flutter_launcher_icons/icon.png',
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.code),
+            title: const Text('View on GitHub'),
+            trailing: const Icon(Icons.open_in_new),
+            onTap: () async {
+              final Uri url = Uri.parse('https://github.com/alex-sandri/ttrt');
+
+              if (await canLaunchUrl(url)) {
+                await launchUrl(url, mode: LaunchMode.externalApplication);
+              }
+            },
+          ),
+          FutureBuilder<PackageInfo>(
+            future: PackageInfo.fromPlatform(),
+            builder: (context, snapshot) {
+              final PackageInfo? packageInfo = snapshot.data;
+
+              if (packageInfo == null) {
+                return const SizedBox();
+              }
+
+              return Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  '${snapshot.data!.appName} ${snapshot.data!.version}',
+                  style: Theme.of(context).textTheme.caption,
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
